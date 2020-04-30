62302d0bd71957f1d24642c0b656decc8410768c
Miduk Immanuel <miduk.immanuel16@gmail.com>
2020-04-30 13:18:52 +0700

    First Commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+import { Button } from "antd";
+import Dashboard from "./containers/layout/Dashboard";
+
+class App extends Component {
+  render() {
+    return <Dashboard />;
+  }
+}
+
+export default App;
